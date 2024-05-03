ed09d5ff4507266b1e6250415b1f0adaf7e8286c
NelsonDJCR <dev@nelsondjcr.com>
2024-05-03 09:55:15 -0500

    add info

---
+++ src/lib/const/INFO.js
@@ -0,0 +1,45 @@
+export const INFO_COMPANY = {
+    APP_NAME: 'Led Office',
+    DOMAIN: 'led-office.com',
+    EMAIL: 'contacto@led-office.com',
+    EMAIL_SEONDARY: 'ventas@led-office.com',
+    EMAIL_THIRD: '',
+    COUNTRY_CODE: '+57',
+    COUNTRY: 'Colombia',
+    CITY: 'Bogotá',
+
+    ABOUT: 'En nuestra empresa, nos enorgullece ofrecer una amplia gama de soluciones de iluminación LED para hogares y proyectos arquitectónicos. Nos dedicamos a proporcionar productos de alta calidad que no solo ofrecen un rendimiento excepcional, sino que también están diseñados para promover la eficiencia energética y la sostenibilidad ambiental',
+    SMALL_ABOUT: 'Empresa dedicada a la venta de iluminación Led para tu hogar y tus proyectos arquitectónicos',
+    OBJETIVE: 'Comercializar al mayor o detal productos de iluminación Led.',
+    MISION: 'Ofrecer productos de iluminación Led para cumplir las necesidades de nuestros clientes y así mismo para contribuir a el cuidado del medio ambiente.',
+    VISION: 'Ser una empresa en 2 años catalogada en el sector de la iluminación led por la alta calidad en su productos y el buen servicio brindado a sus clientes .',
+    ADDRESS: 'Carrera 13 64-63 chapinero Bogotá centro comercial multicompras',
+    SCHEDULE: 'Lunes a viernes 8am a 6 pm Y sábados de 8am a 4 pm los domingos no trabajamos',
+    PHONE: '3502043006',
+    PHONE_SEONDARY: '',
+    LEGAL_REPRESENTATIVE: 'Leandro Lizarazo',
+    LEGAL_REPRESENTATIVE_PHONE: '3023792124',
+}
+export const MENU = [
+    {
+        name: 'Inicio',
+        path: '/',
+    },
+    {
+        name: 'Tienda',
+        path: '/store',
+    },
+    {
+        name: 'Nosotros',
+        path: '/about',
+    },
+    {
+        name: 'Contacto',
+        path: '/contact',
+    },
+]
+export const SOCIAL = {
+    INSTAGRAM: 'https://www.instagram.com/led_office_',
+    FACEBOOK: 'https://www.facebook.com/profile.php?id=100087568515362',
+    WHATSAPP: 'https://wa.me/+53502043006',
+}
